0e5b44bacc7cc651979f088163208d58bb7387f8
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-02-20 13:54:30 +0100

    mui5 + add conditional display

--- apppeinture/src/component/navbar.tsx
+++ apppeinture/src/component/navbar.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-    const toto = () => {
+    const [isDisplayAvion, setIsDisplayAvion] = useState<boolean>(true)
+    const [isDisplayMaquette, setIsDisplayMaquette] = useState<boolean>(true)
+    const [isDisplayPeinture, setIsDisplayPeinture] = useState<boolean>(true)
+    const handleDisplayButton = (event) => {
+        const value = event.currentTarget
+        if (value === 'avion') {
+            setIsDisplayMaquette(true)
+            setIsDisplayPeinture(true)
+            setIsDisplayAvion(false)
 
+        } else if (value === 'maquette') {
+            setIsDisplayMaquette(false)
+            setIsDisplayAvion(true)
+            setIsDisplayPeinture(true)
+        }
+        setIsDisplayMaquette(true)
+        setIsDisplayAvion(true)
+        setIsDisplayPeinture(false)
     }
     return (
         <div id='navbar'>
-            <NavLink  to='/'><img src="" alt="avion"/></NavLink>
-            <NavLink  to='/peinture'><img src="" alt="peinture"/></NavLink>
-            <NavLink  to='/maquette'><img src="" alt="maquette"/></NavLink>
+            {isDisplayAvion && <NavLink to='/' onClick={handleDisplayButton}><img src="" alt="avion"/></NavLink>}
+            {isDisplayPeinture && <NavLink to='/peinture' onClick={handleDisplayButton}><img src="" alt="peinture"/></NavLink>}
+            {isDisplayMaquette && <NavLink to='/maquette' onClick={handleDisplayButton}><img src="" alt="maquette"/></NavLink>}
         </div>
     );
 };
