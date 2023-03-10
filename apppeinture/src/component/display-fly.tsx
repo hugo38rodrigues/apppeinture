1dac37b822ba9531c23902cf6980de8b4b275e9e
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-03-10 10:43:42 +0100

    add css homePage

--- apppeinture/src/component/display-fly.tsx
+++ apppeinture/src/component/display-fly.tsx
@@ -7,7 +7,7 @@ interface DisplayFlyprops {
 const DisplayFly = (prop: DisplayFlyprops) => {
     const {displayFly} = prop
     return (
-        <section>
+        <div className="displayFly">
             {displayFly.map((item) => (
                 <div className='fly' key={item.id}>
                     <h2>{item.title}</h2>
@@ -15,7 +15,7 @@ const DisplayFly = (prop: DisplayFlyprops) => {
 
                 </div>
             ))}
-        </section>
+        </div>
     );
 };
 
