f6956b232e6ba19c7adc848697a6ec3eab03fabc
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-04-11 14:34:34 +0200

    maquette

--- apppeinture/src/page/maquette.tsx
+++ apppeinture/src/page/maquette.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import NavBar from '../component/nav-bar';
+import {Button, FormControl, Input, TextField, Typography} from '@mui/material';
 
 
 const Maquette = () => {
 
     return (
         <div className='page'>
-            <NavBar />
+            <NavBar/>
+            <div className='content-form'>
+                {/*<FormControl >*/}
+                <Typography> Ajouter votre maquette</Typography>
+
+                <Input color="primary" type='text' placeholder='Titre de la maquette'></Input>
+                <Input placeholder="Type in here…" color="primary"/>
+                <Input type='text'></Input>
+
+                <Input type='file' sx={{width: 150, height: 150}}/>
+
+
+                {/*</FormControl>*/}
+            </div>
         </div>
     );
 };
