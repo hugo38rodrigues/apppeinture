7002aa4db6ff2353708d16f4057cff2e71bbda0e
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-07-31 16:59:12 +0200

    css maquette

--- apppeinture/src/component/maquette-form/maquette-form.tsx
+++ apppeinture/src/component/maquette-form/maquette-form.tsx
@@ -1,7 +1,6 @@
 import {
     Box,
     Button,
-    FormControl,
     InputLabel,
     MenuItem,
     Select,
@@ -20,13 +19,11 @@ const MaquetteForm = () => {
     };
 
     return (
-        <Box className='maquette-content'>
-            <Typography variant='h4'> Ajouter votre maquette</Typography>
-            <Box sx={{
-
-            }}>
-                <Box sx={{}}>
-                    <label htmlFor="upload-photo">
+        <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <Box className='maquette-content'>
+                <Typography variant='h4' marginTop={4}> Ajouter votre maquette</Typography>
+                <Box sx={{display: 'flex'}}>
+                    <label htmlFor="upload-photo" className="label-picture">
                         <input
                             style={{display: 'none'}}
                             id="upload-photo"
@@ -37,31 +34,36 @@ const MaquetteForm = () => {
                             color="secondary"
                             variant="outlined"
                             component="span"
-                            sx={{width: 200, height: 200}}
+                            sx={{width: 227, height: 200, marginTop: 4}}
                         >
                             Choisissez votre image
                         </Button>
                     </label>
-                    <TextField color="primary" type='text' placeholder='Titre de la maquette'/>
+                    <Box>
+                        <Box className='form-input'>
+                            <TextField color="primary" type='text' placeholder='Titre de la maquette'/>
+                            <Box sx={{margin: 2}}>
+                                <InputLabel  id="select-paint" className='label-select-paint'>Marque de pot de peinture</InputLabel>
+                                <Select
+                                    labelId="select-paint"
+                                    id="select-paint"
+                                    value={marque}
+                                    label="Age"
+                                    onChange={handleChange}
+                                    fullWidth
+                                >
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </Box>
+                        </Box>
+                        <Box sx={{display:'flex', flexDirection:'column', height: 30, justifyContent: 'space-evenly', alignItems: 'flex-end'}}>
+                            <Button color="primary" variant="contained">Ajout des pots de peintures </Button>
+                            <Button color="success" variant="contained">Crée la maquette</Button>
+                        </Box>
+                    </Box>
                 </Box>
-                <FormControl>
-                    <InputLabel id="demo-simple-select-label">Marque de pot de peinture</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={marque}
-                        label="Age"
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </FormControl>
-
-
-                <Button color="primary" variant="contained">Ajout des pots de peintures </Button>
-                <Button color="success" variant="contained">Crée la maquette</Button>
             </Box>
         </Box>
     );
